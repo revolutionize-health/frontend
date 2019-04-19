be52c6163026d762440358a8d1e009c56c736843
Brandon Allison <brandona8128@outlook.com>
2019-04-19 12:19:22 -0400

    changes to search

--- rev-health/src/Components/ProcedureLookup.js
+++ rev-health/src/Components/ProcedureLookup.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import ProcedureList from './ProcedureList'
 
+
+const input = {
+    border: 'none',
+    borderBottom: '2px solid black',
+    width: '300px',
+    height: '25px',
+    
+    margin:'5px 10px'
+}
+
 class Procedures extends Component {
     constructor(){
         super()
@@ -13,7 +23,7 @@ class Procedures extends Component {
         console.log(window.location.pathname)
         return(
             <div>
-                <input type="text" placeholder="Search"  />
+                <input style={input} type="text" placeholder="Search"  />
                 <ProcedureList/>
             </div>
         )
